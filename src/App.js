24787a07a6f4ef809fbd4083b84a44e9b37783f9
nplazas4 <45074627+nplazas4@users.noreply.github.com>
2020-02-21 17:58:00 -0500

    Prettier code y quitar imports.
    Mejorar la estructura del código.
    
    Quitar imports imnecesarios.
    
    Mover imports de los CSS al archivo de index.js.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-import React from 'react';
-import './materialize-core.css';
-import './theme-verano.css';
-import './all.min.css';
-import Routes from './Routes';
-import Menu from './components/Menu';
-import Login from './pages/Login';
-import Providers from './pages/Providers';
-import Interfaces from './pages/Interfaces';
-import Businessflow from './pages/Businessflow';
-import Dashboard from './pages/Dashboard';
-import Users from './pages/Users';
-
-const exitOption = {
-  borderTop: '1px solid #ddd',
-  borderBottomLeftRadius: '5px',
-  borderBottomRightRadius: '5px'
-}
+import React from "react";
+import Routes from "./Routes";
+import Menu from "./components/Menu";
 
 function App() {
-  console.log(exitOption);
   return (
     <div className="App">
       {/* Global Messages --> */}
-      <div className="global-msj-feedback msj-good" style={{display:'none'}}>
+      <div className="global-msj-feedback msj-good" style={{ display: "none" }}>
         <i className="fas fa-check-circle"></i> Cambios Guardados
       </div>
-      <div className="global-msj-feedback msj-bad" style={{display:'none'}}>
+      <div className="global-msj-feedback msj-bad" style={{ display: "none" }}>
         <i className="fas fa-exclamation-circle"></i> Hubo un error
       </div>
-      
-      <section id='master-container'>
-        {/* Component navitator-bar */}
-        <section className="row" style={{marginBottom:'0'}}>
-          <Menu/>
-          {/* <Login/> */}
-          <Routes/>
+
+      <section id="master-container">
+        <section className="row" style={{ marginBottom: "0" }}>
+          <Menu />
+          <Routes />
         </section>
       </section>
     </div>
