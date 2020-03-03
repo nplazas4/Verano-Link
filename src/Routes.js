a158b683660f59cdc81c7a6fc93e59f80100cb98
nplazas4 <45074627+nplazas4@users.noreply.github.com>
2020-03-03 13:14:43 -0500

    Componote Not Found
    Componente de "página no encontrada", con sus respectivos estilos CSS.

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Providers from "./pages/Providers";
 import Interfaces from "./pages/Interfaces";
@@ -8,33 +8,31 @@ import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import Audit from "./pages/Audit";
 import Config from "./pages/Config";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 import Layout2 from "./components/Layout2";
 
 const Routes = () => {
   return (
+    // <Router>
     <Switch>
-      <React.Fragment>
-        <Route exact path="/veranolink" component={Login} />
-        {/* <Layout> */}
-        {/* <section className="col l10 m12 s12  offset-l2 relative">
+      <Route exact path="/veranolink" component={Login} />
+      {/* <Layout> */}
+      {/* <section className="col l10 m12 s12  offset-l2 relative">
           // <Layout2> */}
-        <Route exact path="/veranolink/proveedores" component={Providers} />
-        <Route exact path="/veranolink/interfaces" component={Interfaces} />
-        <Route
-          exact
-          path="/veranolink/flujos-negocio"
-          component={Businessflow}
-        />
-        <Route exact path="/veranolink/monitoreo" component={Dashboard} />
-        <Route exact path="/veranolink/usuarios" component={Users} />
-        <Route exact path="/veranolink/auditoria" component={Audit} />
-        <Route exact path="/veranolink/configuracion" component={Config} />
-        {/* </Layout2>
+      <Route exact path="/veranolink/proveedores" component={Providers} />
+      <Route exact path="/veranolink/interfaces" component={Interfaces} />
+      <Route exact path="/veranolink/flujos-negocio" component={Businessflow} />
+      <Route exact path="/veranolink/monitoreo" component={Dashboard} />
+      <Route exact path="/veranolink/usuarios" component={Users} />
+      <Route exact path="/veranolink/auditoria" component={Audit} />
+      <Route exact path="/veranolink/configuracion" component={Config} />
+      <Route component={NotFound} />
+      {/* </Layout2>
         </section> */}
-      </React.Fragment>
       {/* </Layout> */}
     </Switch>
+    // </Router>
   );
 };
 
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+import "../components/styles/NotFound.css";
 
-function NotFound() {
-  return <h1>404: Not Found</h1>;
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div id="notfound">
+        <div class="notfound-bg"></div>
+        <div class="notfound">
+          <div class="notfound-404">
+            <h1>404</h1>
+          </div>
+          <h2>Lo sentimos, pero la página solicitada no ha sido encontrada.</h2>
+          <Link to="/veranolink/proveedores" class="home-btn">
+            Inicio
+          </Link>
+          {/* <Link to="#" class="contact-btn">
+            Contáctenos
+          </Link> */}
+          <div class="notfound-social">
+            {/* <Footer /> */}
+            {/* <footer class="tc white-text">
+              Creado por <b>Verano 2020</b>
+            </footer> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotFound;
